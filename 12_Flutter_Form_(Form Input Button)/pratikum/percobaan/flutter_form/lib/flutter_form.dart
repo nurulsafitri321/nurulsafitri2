50481418f2fc89250a224085c6b5e9026a6300ca
nurulsafitri321 <nrlsftr161@gmail.com>
2023-09-16 06:23:05 +0700

    screenshoot percobaan pratikum

--- 12_Flutter_Form_(Form Input Button)/pratikum/percobaan/flutter_form/lib/flutter_form.dart
+++ 12_Flutter_Form_(Form Input Button)/pratikum/percobaan/flutter_form/lib/flutter_form.dart
@@ -10,6 +10,9 @@ class FlutterForm extends StatefulWidget {
 class _FlutterFormState extends State<FlutterForm> {
   var formKey = GlobalKey<FormState>();
   var inputController = TextEditingController();
+  var radioValue = '';
+  var checkValue = false;
+  var dropdownValue = 0;
   @override
 
   void dispose() {
@@ -27,7 +30,92 @@ class _FlutterFormState extends State<FlutterForm> {
             children: [
               TextField(
                 controller: inputController,
-              )
+              ),
+              ListTile(
+                leading: Radio<String> (
+                  value: 'Laki-laki',
+                  groupValue: radioValue,
+                  onChanged: (String? value) {
+                    setState(() {
+                      radioValue = value ?? '';
+                    }
+                    );
+                  }
+              ),
+              title: const Text('Laki-laki'),
+              ),
+
+              ListTile(
+                leading: Radio<String> (
+                  value: 'Perempuan',
+                  groupValue: radioValue,
+                  onChanged: (String? value) {
+                    setState(() {
+                      radioValue = value ?? '';
+                    }
+                    );
+                  }
+              ),
+              title: const Text('Perempuan'),
+              ),
+              ListTile(
+                leading: Checkbox(
+                  value: checkValue,
+                  onChanged: (bool? value){
+                setState(() {
+                  checkValue = value ?? false;
+                });
+              },
+            ),
+            title: const Text('Setuju/ Tidak Setuju'),
+            ),
+            DropdownButton(
+              value: dropdownValue,
+              onChanged: (int? value) {
+                setState(() {
+                  dropdownValue = value ?? 0;
+                });
+              },
+              items: const [
+                DropdownMenuItem(
+                  value: 0, 
+                  child: Text('Pilih....'),
+                ),
+
+                DropdownMenuItem(
+                  value: 1, 
+                  child: Text('satu'),
+                ),
+
+                DropdownMenuItem(
+                  value: 2, 
+                  child: Text('dua'),
+                ),
+
+                DropdownMenuItem(
+                  value: 3, 
+                  child: Text('tiga'),
+                ),
+
+                DropdownMenuItem(
+                  value: 4, 
+                  child: Text('empat'),
+                ),
+
+                DropdownMenuItem(
+                  value: 5, 
+                  child: Text('lima'),
+                ),
+              ], 
+              ),
+              ElevatedButton(
+                onPressed: () {}, 
+                child: const Text ('Submit'),
+              ),
+              IconButton(
+                onPressed: () {}, 
+                icon: Icon(Icons.add),
+              ),
             ],
           ),
           )
