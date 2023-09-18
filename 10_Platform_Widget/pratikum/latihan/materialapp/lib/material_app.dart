d3b2a5131045f122de0cdd25722017d7a47b9d88
nurulsafitri321 <nrlsftr161@gmail.com>
2023-09-18 08:03:30 +0700

    platform_widget

--- 10_Platform_Widget/pratikum/latihan/materialapp/lib/material_app.dart
+++ 10_Platform_Widget/pratikum/latihan/materialapp/lib/material_app.dart
@@ -13,3 +13,6 @@ class App extends StatelessWidget {
     );
   }
 }
+
+
+
--- 10_Platform_Widget/pratikum/latihan/materialapp/lib/material_page.dart
+++ 10_Platform_Widget/pratikum/latihan/materialapp/lib/material_page.dart
@@ -1,93 +1,89 @@
-import 'package:flutter/material.dart';
+// 
+
+//Cupertino
+
+import 'package:flutter/cupertino.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('MaterialApp'),
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: const Text('CupertinoApp'),
       ),
-
-    drawer: Drawer(
       child: ListView(
-        children: const [
-          ListTile(title: Text("Home")),
-          ListTile(title: Text("TextSetting"))
-        ],
-      ),
-    ),
-    body : ListView(
-        children: [
-          ListTile(
-            leading: Icon(Icons.circle, color: Colors.green,),
+        children: <Widget>[
+          CupertinoListTile(
+            leading: Icon(CupertinoIcons.circle_filled, color: CupertinoColors.systemGreen),
             title: Text('Leane Graham'),
             subtitle: Text('1-770-736-8031 x56442'),
             onTap: () {
               // Tambahkan aksi ketika ListTile ditekan di sini
             },
           ),
-          ListTile(
-            leading: Icon(Icons.star),
+          CupertinoListTile(
+            leading: Icon(CupertinoIcons.circle_filled, color: CupertinoColors.systemGreen),
             title: Text('Ervin Howell'),
             subtitle: Text('010-692-6593 x09125'),
             onTap: () {
               // Tambahkan aksi ketika ListTile ditekan di sini
             },
           ),
-
-          ListTile(
-            leading: Icon(Icons.star),
+          CupertinoListTile(
+            leading: Icon(CupertinoIcons.circle_filled, color: CupertinoColors.systemGreen),
             title: Text('Clementine Bauch'),
             subtitle: Text('1-463-123-4447'),
             onTap: () {
               // Tambahkan aksi ketika ListTile ditekan di sini
             },
           ),
-
-          ListTile(
-            leading: Icon(Icons.star),
+          CupertinoListTile(
+            leading: Icon(CupertinoIcons.circle_filled, color: CupertinoColors.systemGreen),
             title: Text('Patricia Lebsack'),
             subtitle: Text('493-170-9623 x156'),
             onTap: () {
               // Tambahkan aksi ketika ListTile ditekan di sini
             },
           ),
-
-          ListTile(
-            leading: Icon(Icons.star),
+          CupertinoListTile(
+            leading: Icon(CupertinoIcons.circle_filled, color: CupertinoColors.systemGreen),
             title: Text('Chelsey Dietrich'),
             subtitle: Text('(256)954-1289'),
             onTap: () {
               // Tambahkan aksi ketika ListTile ditekan di sini
             },
           ),
-
-          ListTile(
-            leading: Icon(Icons.star),
-            title: Text('Mrs.Dennis Schulist'),
+          CupertinoListTile(
+            leading: Icon(CupertinoIcons.circle_filled, color: CupertinoColors.systemGreen),
+            title: Text('Mrs. Dennis Schulist'),
             subtitle: Text('1-477-935-8478 x6430'),
             onTap: () {
               // Tambahkan aksi ketika ListTile ditekan di sini
             },
           ),
-
-          ListTile(
-            leading: Icon(Icons.star),
+          CupertinoListTile(
+            leading: Icon(CupertinoIcons.circle_filled, color: CupertinoColors.systemGreen),
             title: Text('Kurtis Weissnat'),
             subtitle: Text('210.068.6132'),
             onTap: () {
               // Tambahkan aksi ketika ListTile ditekan di sini
             },
           ),
-          // Tambahkan lebih banyak ListTile di sini sesuai kebutuhan
+          // Tambahkan lebih banyak CupertinoListTile di sini sesuai kebutuhan
         ],
       ),
-    bottomNavigationBar: BottomNavigationBar(
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings')
-      ],
-    ),
+      bottomNavigationBar: CupertinoTabBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
     );
   }
 }
