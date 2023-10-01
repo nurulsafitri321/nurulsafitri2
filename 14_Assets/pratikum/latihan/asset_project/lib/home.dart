d543614651e9d21e55d72e87a4ec88fc5d29623f
Nurul Safitri <nrlsftr161@gmail.com>
2023-10-01 14:54:35 +0700

    tugas latihan pratikum assest bagian prioritas1

--- 14_Assets/pratikum/latihan/asset_project/lib/home.dart
+++ 14_Assets/pratikum/latihan/asset_project/lib/home.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('GridView'),
+        title: const Text('List Galeri'),
       ),
       body: const ImageGridView(),
     );
