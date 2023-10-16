4bb806e31c51bb0bcb764c4a34e67ccdd3476165
Nurul Safitri <nrlsftr161@gmail.com>
2023-10-16 10:44:32 +0700

    latihan pratikum yang ada di dlam vidio state management bagian global state

--- 17_Flutter_State_Management_(Provider)/pratikum/percobaan/phonebook_app/lib/home_page.dart
+++ 17_Flutter_State_Management_(Provider)/pratikum/percobaan/phonebook_app/lib/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:phonebook_app/model.dart';
+import 'package:provider/provider.dart';
+import 'package:phonebook_app/stores/contact.dart' as contact_store;
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +12,71 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String name = '';
+  String phoneNumber = '';
+  var nameController = TextEditingController();
+  var phoneNumberController = TextEditingController();
+  var formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    phoneNumberController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final contactProvider = Provider.of<contact_store.Contact>(context);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Phone Book App'),),
+        title: const Text('Phone Book App'),
+      ),
+      body: Form(
+        key: formKey, 
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              onChanged: (String value) {
+                name = value;
+              },
+            ),
+            TextField(
+              controller: phoneNumberController,
+              onChanged: (String value) {
+                phoneNumber = value;
+              },
+            ),
+            ElevatedButton(
+              onPressed: (){
+                if(!formKey.currentState!.validate()) return;
+
+                formKey.currentState!.save();
+
+                contactProvider.add(
+                  GetContact(
+                    name: name, 
+                    phoneNumber: phoneNumber,
+                  ),
+                );
+              },
+              child: const Text('Add Number'),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: contactProvider.Contacts.length,
+                itemBuilder: (context, index){
+                  return ListTile(
+                    title: Text(contactProvider.Contacts[index].name),
+                    subtitle: Text(contactProvider.Contacts[index].phoneNumber),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
