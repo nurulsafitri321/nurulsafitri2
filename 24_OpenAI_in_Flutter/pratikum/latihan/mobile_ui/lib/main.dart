cbdad78e4d7bc52e17c65da36e1884934cafe989
Nurul Safitri <nrlsftr161@gmail.com>
2023-10-21 09:24:17 +0700

    smartphone_app

--- 24_OpenAI_in_Flutter/pratikum/latihan/mobile_ui/lib/main.dart
+++ 24_OpenAI_in_Flutter/pratikum/latihan/mobile_ui/lib/main.dart
@@ -1,22 +1,44 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:mobile_ui/halaman_hasil.dart';
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+// import 'package:mobile_ui/halaman_hasil.dart';
 
-import 'home_page.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Rekomendasi Smartphone',
-      home: HomePage(),
-      routes: {
-        '/hasil': (context) => HasilPage(),
-      },
+    return const MaterialApp(
+      title: 'rekomendasi smartphone',
+      home: PhoneRecommendation(),
     );
   }
 }
+
+
+
+
+
+// import 'home_page.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Rekomendasi Smartphone',
+//       home: HomePage(),
+//       routes: {
+//         '/hasil': (context) => HasilPage(),
+//       },
+//     );
+//   }
+// }
