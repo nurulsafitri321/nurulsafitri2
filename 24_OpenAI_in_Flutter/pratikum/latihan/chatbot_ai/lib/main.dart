9eebab7f55a67b37eb3eb3888da14de79ee62835
Nurul Safitri <nrlsftr161@gmail.com>
2023-11-06 14:56:07 +0700

    commit openai

---
+++ 24_OpenAI_in_Flutter/pratikum/latihan/chatbot_ai/lib/main.dart
@@ -0,0 +1,19 @@
+
+import 'package:chatbot_ai/ui/chat_body.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ChatPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
